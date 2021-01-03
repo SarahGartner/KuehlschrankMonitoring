2c71bef7dd2fce326166e0d3a0ad8d34b3a7544a
SarahGartner <sarah.gartner97@gmail.com>
2021-01-04 00:25:55 +0100

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 require('dotenv/config');
 
@@ -10,10 +11,12 @@ const kuehlgeraeteRoutes = require('./routes/kuehlgeraeteRoutes');
 //JSON aus Body auslesen
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 //Routen
 app.use('/sensordata', sensordatenRoutes);
 app.use('/fridges', kuehlgeraeteRoutes);
+
 
 //Mit Datenbank verbinden
 mongoose.connect(process.env.DB,
