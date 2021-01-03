475af7158dffb4face6f88fc7318fb12ec9b3e15
SarahGartner <sarah.gartner97@gmail.com>
2021-01-03 13:16:10 +0100

    neue DB nach unabsichtlichem Löschen, auslesen aus .env

--- app.js
+++ app.js
@@ -7,20 +7,17 @@ require('dotenv/config');
 const webappRoutes = require('./routes/webapp');
 const microRoutes = require('./routes/microcontroller');
 
-
 //JSON aus Body auslesen
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use('/webapp', webappRoutes);
 app.use('/micro', microRoutes);
 
-
-mongoose.connect('mongodb://kuehlschrankmonitoring:1kFDVoKr8zHrVubhpjAe3jeYNMowxy4ZoAfDyKNOyTy8sMXs0r0gVd8rmB4ppK4ZBJQOFi3th0oEi8gEJoj6JA==@kuehlschrankmonitoring.mongo.cosmos.azure.com:10255/?ssl=true&appName=@kuehlschrankmonitoring@',
+//Mit Datenbank verbinden
+mongoose.connect(process.env.DB,
     { useUnifiedTopology: true, useNewUrlParser: true, retrywrites: false }, () =>
     console.log('Mit Kühlschrankmonitoring MongoDB verbunden')
 );
-
 
 app.listen(port);
