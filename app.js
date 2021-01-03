31f094be673e94d6cd9c3d52ba830221eec38d2e
Sarah Gartner <it201508@fhstp.ac.at>
2021-01-03 11:22:38 +0100

    aufräumen

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const webappRoutes = require('./routes/webapp');
 const microRoutes = require('./routes/microcontroller');
 
 
-//Middleware
+//JSON aus Body auslesen
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,30 +16,6 @@ app.use('/webapp', webappRoutes);
 app.use('/micro', microRoutes);
 
 
-
-// app.use('/posts', () => {
-//     console.log('posts was called');
-// });
-// app.use('/', () => {
-//     console.log('/ was called');
-// });
-// var mongoClient = require("mongodb").MongoClient;
-// mongoClient.connect("mongodb://kuehlschrankmonitoring:1kFDVoKr8zHrVubhpjAe3jeYNMowxy4ZoAfDyKNOyTy8sMXs0r0gVd8rmB4ppK4ZBJQOFi3th0oEi8gEJoj6JA==@kuehlschrankmonitoring.mongo.cosmos.azure.com:10255/?ssl=true&appName=@kuehlschrankmonitoring@", function (err, db) {
-//   db.close();
-// });
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('HELLO');
-// });
-
-// app.get('/posts', (req, res) => {
-//     res.send('HELLO posts');
-// });
-
-
-// mongoose.connect(process.env.DB_CONNECTION,
 mongoose.connect('mongodb://kuehlschrankmonitoring:1kFDVoKr8zHrVubhpjAe3jeYNMowxy4ZoAfDyKNOyTy8sMXs0r0gVd8rmB4ppK4ZBJQOFi3th0oEi8gEJoj6JA==@kuehlschrankmonitoring.mongo.cosmos.azure.com:10255/?ssl=true&appName=@kuehlschrankmonitoring@',
     { useUnifiedTopology: true, useNewUrlParser: true, retrywrites: false }, () =>
     console.log('Mit Kühlschrankmonitoring MongoDB verbunden')
