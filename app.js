24bb056a606063ed2f40fade17e3daeb99dd94b3
SarahGartner <sarah.gartner97@gmail.com>
2021-01-05 20:51:17 +0100

    mqtt funkt: spaghetti - russen - code

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 const port = process.env.PORT || 3000;
+const https = require('https');
 require('dotenv/config');
+const Sensordaten = require('./models/Sensordaten');
+const Kuehlgeraet = require('./models/Kuehlgeraete');
 
 const sensordatenRoutes = require('./routes/sensordatenRoutes');
 const kuehlgeraeteRoutes = require('./routes/kuehlgeraeteRoutes');
@@ -25,3 +28,87 @@ mongoose.connect(process.env.DB,
 );
 
 app.listen(port);
+
+
+
+//MQTT
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://test.mosquitto.org')
+user = [201501, 201508]
+
+
+//bei User/save auch aufrufen!! 
+//subscribe
+user.forEach(u => {
+    console.log(u)
+    client.on('connect', function () {
+        client.subscribe(u + '/#', function (err) {
+            if (!err) {
+            }
+        })
+    })
+})
+
+//saveSensordata
+client.on('message', function (topic, message) {
+    const userId = topic.split('/')[0];
+    const crossgateId = topic.split('/')[1];
+    const sensorMac = '555';
+    const temperature = 4.4;
+    const humidity = 40.6;
+    const sensordata = [{
+        "sensorMac": "MQTTTest",
+        "temperature": 4.1,
+        "humidity": 80,
+        "userId": userId,
+        "crossGateId": "44-55-33"
+    }];
+    // const timestamp = "";
+    // console.log(message[0]['temp'].toString());
+    // console.log(sensorMac);
+    console.log(topic + ': ' + message);
+
+    const sensordaten = [];
+    const fridges = [];
+    sensordata.forEach(e =>
+        sensordaten.push(
+            new Sensordaten({
+                _id: {
+                    sensorMac: e.sensorMac,
+                    timestamp: e.timestamp
+                },
+                temperature: e.temperature,
+                humidity: e.humidity,
+                userId: e.userId,
+                crossGateId: e.crossGateId
+            })
+        )
+    )
+    try {
+        const savedSensordaten = [];
+        sensordaten.forEach(e =>
+            e.save());
+        sensordaten.forEach(e =>
+            savedSensordaten.push(e));
+        console.log(savedSensordaten);
+    } catch (error) {
+        console.log(error);
+    };
+    sensordata.forEach(async e => {
+        try {
+            const kuehlgeraet = await Kuehlgeraet.find({ _id: e.sensorMac });
+            if (kuehlgeraet.length == 0) {
+                await new Kuehlgeraet({
+                    _id: e.sensorMac,
+                    fridgeId: "",
+                    name: "",
+                    userId: e.userId,
+                    crossGateId: e.crossGateId
+                }).save();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    )
+})
